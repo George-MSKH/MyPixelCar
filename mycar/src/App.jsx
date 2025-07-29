6667b911349a6909fe960798046e58af2267d71f
George <meskhoradzegio@gmail.com>
2025-07-29 18:07:03 +0400

    added homepage text

--- mycar/src/App.jsx
+++ mycar/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './Header/Header'
+import Home from './Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Header />
+      <div className="land">
+          <Home />
+      </div>
     </>
   )
 }
--- mycar/src/Home/Home.jsx
+++ mycar/src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Homecss.css'
 
 export default function Home() {
   return (
