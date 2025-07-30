3180c3cb9d7e4d96ad11b22bb68bc1944c3f24aa
Nikoloz Khutsishvili <103494069+NikaXucishvili@users.noreply.github.com>
2025-07-30 16:04:22 +0400

    added logo and search bar

--- mycar/src/Header/Header.jsx
+++ mycar/src/Header/Header.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
-import "./Header.css";
+import './Header.css';
+import { FaSearch } from 'react-icons/fa';
 import HeaderSection from './HeaderSection';
 
 export default function Header() {
   return (
-    <div>
-        <div className="header">
-            <HeaderSection  menuname="Home" link='/' />
-            <HeaderSection  menuname="Cars" link='/cars'/>
-            <HeaderSection  menuname="About" link='/about'/>
-            <HeaderSection  menuname="Contact" link='/contact'/>
-        </div>
+    <div className="header">
+      <div className="header-logo">
+      <h1 className="pixel-logo">MyPixelCar</h1>
+  </div>
+
+
+      <div className="header-links">
+        <HeaderSection menuname="Home" link="/" />
+        <HeaderSection menuname="Cars" link="/cars" />
+        <HeaderSection menuname="About" link="/about" />
+        <HeaderSection menuname="Contact" link="/contact" />
+      </div>
+
+      <div className="searchbar">
+        <input
+          type="search"
+          placeholder="Search Cars"
+          className="pixel-input"
+        />
+        <FaSearch className="search-icon" />
+      </div>
     </div>
-  )
+  );
 }
